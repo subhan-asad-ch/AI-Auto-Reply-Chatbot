4328ad3d238b7c0a3f0937e22ed4be55d247aee2
Muhammad Subhan Asad <123264367+subhan-asad-ch@users.noreply.github.com>
2024-06-24 01:02:55 +0500

    Update cursor-points.py

--- cursor-points.py
+++ cursor-points.py
@@ -1,10 +1,23 @@
 import pyautogui
+import time
+import pyperclip
 
-while True:
-    a = pyautogui.position()
-    print(a)
-    # 1639, 1412
-    # 1003 237 to 2187 1258
-    # 1026 244
-    # to 1131 1321
-# can be different for your screen Dimensions
+def click_on_chrome_icon():
+    pyautogui.click(1639, 1412)
+    time.sleep(1)
+
+def select_text():
+    pyautogui.moveTo(972, 202)
+    pyautogui.dragTo(2213, 1278, duration=2.0, button='left')
+    pyautogui.hotkey('ctrl', 'c')
+    time.sleep(2)
+
+def paste_response():
+    pyautogui.click(1808, 1328)
+    time.sleep(1)
+    pyautogui.hotkey('ctrl', 'v')
+    time.sleep(1)
+    pyautogui.press('enter')
+
+def click_on_input_box():
+    pyautogui.click(1994, 281)
